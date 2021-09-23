ce95bf2e1bf11519989a6528ae4e1575c16a8d59
AbdulMoizChishti <moaiz599@gmail.com>
2021-09-24 01:11:51 +0500

    Functions in java

---
+++ src/functions/A.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package functions;
+
+/**
+ *
+ * @author Abdul Moiz Chishti
+ */
+public class A {
+    
+    int a;
+    public double volume( int x){
+    a=x;
+        System.out.println(a);
+        return 100.9;
+    
+    }
+    
+}
